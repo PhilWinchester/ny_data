130127bc8e66e793e7ded6563cdcadc9d3c33897
PhilWinchester <philipwinc@gmail.com>
2021-09-16 11:54:13 -0400

    remove generated secret key for placeholder

--- ny_data/settings.py
+++ ny_data/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '381c!q62x-5wejfv_ifgml8^mcs28pg@#d$egji$q$!zd0)av='
+SECRET_KEY = 'noSecretKeyYet'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
